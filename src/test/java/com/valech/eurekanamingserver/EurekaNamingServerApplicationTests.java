f60ac4111bfdf9e388cdbb5f30588ce104657ca4
marellarambabu <marellarambabu@gmail.com>
2021-07-19 09:56:09 +0530

    commit

---
+++ src/test/java/com/valech/eurekanamingserver/EurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valech.eurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
